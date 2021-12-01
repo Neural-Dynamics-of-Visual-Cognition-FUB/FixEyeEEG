a495f0d71ead517554084b844adef1af5ae782d9
ghaeberle <g.haeberle@fu-berlin.de>
2021-12-01 14:37:04 +0100

    wrote script to run category decoding on bash

---
+++ scripts/eyetracking/run_SVM_category_curta.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+#SBATCH --mail-user=haebeg19@zedat.fu-berlin.de
+#SBATCH --job-name=category 
+#SBATCH --mail-type=end
+#SBATCH --mem=9000
+#SBATCH --time=10:00:00
+#SBATCH --qos=prio
+
+declare -a permutations
+index=0
+for sub in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 
+do
+    for method in 2 
+do
+    permutations[$index]="$sub $method"
+    index=$((index + 1))
+    done
+done
+#Extract parameters
+params=(${permutations[${SLURM_ARRAY_TASK_ID}]})
+sub=${params[0]}
+
+echo sub $sub
+
+### Set up runtime environment
+
+module add MATLAB/2021a
+
+# wait a bit so it doesn't crash
+sleep 50
+
+cd /home/haebeg19/FixEyeEEG/scripts/EEG/
+
+### Start job
+
+matlab -nosplash -noFigureWindows -r "category_decoding_SVM(${sub})" > serial.out #this worked
+echo set to run
+### Output core and memory efficiency
+
+seff $SLURM_JOBID
+
+
+
