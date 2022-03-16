61b47c860859a2e29d741273e4de72edf0458d88
ghaeberle <g.haeberle@fu-berlin.de>
2022-03-16 13:53:04 +0100

    changed time time decoding to same category

--- scripts/decoding/run_time_time_object_decoding.sh
+++ scripts/decoding/run_time_time_object_decoding.sh
@@ -1,4 +1,4 @@
- #!/bin/bash
+#!/bin/bash
 
 #SBATCH --mail-user=haebeg19@zedat.fu-berlin.de
 #SBATCH --job-name=time_time_objects
@@ -13,9 +13,9 @@ for sub in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27
 do
 	for methods in 1 2
 	do
-		for within in 1 2 
+		for cond in 1 2 
 do
-    permutations[$index]="$sub $methods $within" 
+    permutations[$index]="$sub $methods $cond" 
     index=$((index + 1))
 	done
 done
@@ -24,10 +24,10 @@ done
 params=(${permutations[${SLURM_ARRAY_TASK_ID}]})
 sub=${params[0]}
 methods=${params[1]}
-within=${params[2]}
+cond=${params[2]}
 echo sub $sub
 echo methods $methods
-echo within $within
+echo cond $cond
 ### Set up runtime environment
 
 module add MATLAB/2021a
@@ -39,7 +39,7 @@ cd /home/haebeg19/FixEyeEEG/scripts/decoding/
 
 ### Start job
 
-matlab -nosplash -noFigureWindows -r "time_time_object_decoding(${sub}, ${methods}, ${within})" > serial.out #this worked
+matlab -nosplash -noFigureWindows -r "time_time_object_decoding(${sub}, ${methods}, ${cond})" > serial.out #this worked
 echo set to run
 ### Output core and memory efficiency
 
