633920a894367730828de29fb398ad16b684d201
ghaeberle <g.haeberle@fu-berlin.de>
2022-03-18 13:50:31 +0100

    changed scripts such thatfixatin condition is its own run in slurm

--- scripts/decoding/time_time_category_decoding.m
+++ scripts/decoding/time_time_category_decoding.m
@@ -1,4 +1,4 @@
-function [outputArg1,outputArg2] = time_time_category_decoding(subj, method, within)
+function [] = time_time_category_decoding(subj, fixation_condition, method)
 %{
     - Multivariate Noise Normalisation
     - category decoding for both fixation crosses for animate versus
@@ -41,6 +41,11 @@ n_permutations = 100;
 n_pseudotrials = 6;
 num_conditions = 2; %categories to decode
 
+if fixation_condition == 2
+    fixation_condition = 'standard';
+elseif fixation_condition == 1
+    fixation_condition = 'bulls';
+end 
 % 1 = EEG, 2 = Eyetracking
 if method == 1
     filepath_preprocessed_data = sprintf('%sdata/FixEyeEEG/main/eeg/preprocessed/%s/noICA/preprocessed_noICA_timelocked.mat',BASE,subj);
@@ -66,24 +71,21 @@ end
 
 time_points = size(data.time,2);
 %% split data into standard(2) and bullseye(1) fixation cross
-% standard
-cfg = [];
-cfg.trials = find(data.trialinfo(:,5)=='2');
-data_standard = ft_selectdata(cfg, data);
-
-% bullseye
-cfg = [];
-cfg.trials = find(data.trialinfo(:,5)=='1');
-data_bulls = ft_selectdata(cfg, data);
-
+if strcmp(fixation_condition, 'standard') == 1
+    % standard 
+    cfg = [];
+    cfg.trials = find(preprocessed_data.trialinfo(:,5)=='2');
+    data = ft_selectdata(cfg, preprocessed_data);
+elseif strcmp(fixation_condition, 'bulls') == 1
+    cfg = [];
+    cfg.trials = find(preprocessed_data.trialinfo(:,5)=='1');
+    data = ft_selectdata(cfg, preprocessed_data);
+end  
 % minimum number of trials standard
-number_of_trial_animate_standard = sum(data_standard.trialinfo(:,3)=='1','all');
-number_of_trial_inanimate_standard = sum(data_standard.trialinfo(:,3)=='0','all');
+number_of_trial_animate = sum(data.trialinfo(:,3)=='1','all');
+number_of_trial_inanimate = sum(data.trialinfo(:,3)=='0','all');
 
-number_of_trial_animate_bulls = sum(data_bulls.trialinfo(:,3)=='1','all');
-number_of_trial_inanimate_bulls = sum(data_bulls.trialinfo(:,3)=='0','all');
-
-min_number_of_trials = min([number_of_trial_animate_standard,number_of_trial_inanimate_standard, number_of_trial_animate_bulls,number_of_trial_inanimate_bulls] );
+min_number_of_trials = min([number_of_trial_animate,number_of_trial_inanimate] );
 
 % Preallocate
 decodingAccuracy=NaN(n_permutations,time_points,time_points);
@@ -97,84 +99,61 @@ for perm = 1:n_permutations
     %   number of conditioins, M is the number of trials, E is the number of
     %   electrodes and TP is the number of timepoints.
     
-    data_MVNN_standard = create_data_matrix_MVNN(num_conditions, min_number_of_trials, data_standard, 'category');
-    data_MVNN_bulls = create_data_matrix_MVNN(num_conditions, min_number_of_trials, data_bulls, 'category');
-    
-    
+    data_MVNN = create_data_matrix_MVNN(num_conditions, min_number_of_trials, data_standard, 'category');  
+ 
     % actually do the MVNN
-    [data_MVNN_standard, ~] = multivariate_noise_normalization(data_MVNN_standard);
-    [data_MVNN_bulls, ~] = multivariate_noise_normalization(data_MVNN_bulls);
+    [data_MVNN, ~] = multivariate_noise_normalization(data_MVNN);
     %% split data into animate and inanimate trials
-    data_animate_standard = data_MVNN_standard(1,:,:,:);
-    data_inanimate_standard = data_MVNN_standard(2,:,:,:);
+    data_animate = data_MVNN(1,:,:,:);
+    data_inanimate = data_MVNN(2,:,:,:);
     
-    data_animate_bulls = data_MVNN_bulls(1,:,:,:);
-    data_inanimate_bulls = data_MVNN_bulls(2,:,:,:);
     %% create pseudotrials standard
     disp('Permute the trials')
-    data_animate_standard_permuted = data_animate_standard(:, randperm(size(data_animate_standard,2)),:,:);
-    data_inanimate_standard_permuted = data_inanimate_standard(:,randperm(size(data_inanimate_standard,2)),:,:);
+    data_animate_permuted = data_animate(:, randperm(size(data_animate,2)),:,:);
+    data_inanimate_permuted = data_inanimate(:,randperm(size(data_inanimate,2)),:,:);
     
     disp('Put both categories into one matrix');
-    data_both_categories_standard = NaN([size(data_animate_standard_permuted)]);
-    data_both_categories_standard(1,:,:,:) = data_animate_standard_permuted;
-    data_both_categories_standard(2,:,:,:) = data_inanimate_standard_permuted;
+    data_both_categories = NaN(size(data_animate_permuted));
+    data_both_categories(1,:,:,:) = data_animate_permuted;
+    data_both_categories(2,:,:,:) = data_inanimate_permuted;
     
     disp('Split into pseudotrials');
     num_trials_per_bin = round(min_number_of_trials/n_pseudotrials);
-    pseudo_trials_standard = create_pseudotrials(num_conditions, num_trials_per_bin, n_pseudotrials, data_both_categories_standard);
+    pseudo_trials = create_pseudotrials(num_conditions, num_trials_per_bin, n_pseudotrials, data_both_categories);
     
-    %% create pseudotrials bulls
-    disp('Permute the trials')
-    data_animate_bulls_permuted = data_animate_bulls(:, randperm(size(data_animate_bulls,2)),:,:);
-    data_inanimate_bulls_permuted = data_inanimate_bulls(:,randperm(size(data_inanimate_bulls,2)),:,:);
-    
-    disp('Put both categories into one matrix');
-    data_both_categories_bulls = NaN([size(data_animate_bulls_permuted)]);
-    data_both_categories_bulls(1,:,:,:) = data_animate_bulls_permuted;
-    data_both_categories_bulls(2,:,:,:) = data_inanimate_bulls_permuted;
-    
-    disp('Split into pseudotrials');
-    num_trials_per_bin = round(min_number_of_trials/n_pseudotrials);
-    pseudo_trials_bulls = create_pseudotrials(num_conditions, num_trials_per_bin, n_pseudotrials, data_both_categories_bulls);
-    
-    if within == 0 
-        training_data = pseudo_trials_standard;
-        testing_data = pseudo_trials_bulls;
-        filename = 'standard_bulls_category';
-    elseif within == 1
-        training_data = pseudo_trials_bulls;
-        testing_data = pseudo_trials_standard;  
-        filename = 'bulls_standard_category';
-    end
     %% do the actual decoding
     for time1 = 1:time_points
         
         %% standard
         % split into trainung and testing
-        training_data_standard=[squeeze(training_data(1,1:end-1,:,time1)) ; squeeze(training_data(2,1:end-1,:,time1))];
+        training_data=[squeeze(pseudo_trials(1,1:end-1,:,time1)) ; squeeze(pseudo_trials(2,1:end-1,:,time1))];
         % create labels for the SVM
-        labels_train_standard  = [ones(1,n_pseudotrials-1) 2*ones(1,n_pseudotrials-1)];
+        labels_train = [ones(1,n_pseudotrials-1) 2*ones(1,n_pseudotrials-1)];
         
         disp('Train the SVM');
         train_param_str=  '-s 0 -t 0 -b 0 -c 1 -q';
-        model_standard=svmtrain(labels_train_standard',training_data_standard,train_param_str);
+        model = svmtrain(labels_train',training_data,train_param_str);
         for time2 = 1:time_points
-            testing_data_bulls=[squeeze(testing_data(1,end,:,time2))' ; squeeze(testing_data(2,end,:,time2))'];
+            testing_data=[squeeze(pseudo_trials(1,end,:,time2))' ; squeeze(pseudo_trials(2,end,:,time2))'];
             labels_test   = [1 2];
             
             disp('Test the SVM');
-            [~, accuracy_standard, ~] = svmpredict(labels_test',testing_data_bulls,model_standard);
-            decodingAccuracy(perm,time1,time2)=accuracy_standard(1);
+            [~, accuracy, ~] = svmpredict(labels_test',testing_data,model);
+            decodingAccuracy(perm,time1,time2)=accuracy(1);
         end
     end
 end
 
 %% Save the decision values + decoding accuracy
-decodingAccuracy_avg = squeeze(mean(decodingAccuracy,1));
-save(fullfile(results_dir,sprintf('%s_decodingAccuracy_time_time.mat',filename)),'decodingAccuracy_avg');
-save(fullfile(results_dir,sprintf('%s_decodingAccuracy_min_number_trials.mat',filename)),'min_number_of_trials');
-
+ if strcmp(fixation_condition, 'standard') == 1
+    decodingAccuracy_avg_standard = squeeze(mean(decodingAccuracy,1));
+    filename = sprintf('category_%s',fixation_condition);
+    save(fullfile(results_dir,sprintf('%s_time_time_avg.mat',filename)),'decodingAccuracy_avg_standard');
+ elseif strcmp(fixation_condition, 'bulls') == 1
+    decodingAccuracy_avg_bulls = squeeze(mean(decodingAccuracy,1));
+    filename = sprintf('category_%s',fixation_condition);
+    save(fullfile(results_dir,sprintf('%s_time_time_avg.mat',filename)),'decodingAccuracy_avg_bulls');
+ end 
 end
 
 
