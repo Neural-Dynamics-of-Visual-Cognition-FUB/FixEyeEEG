919305cae841e7418e374ae11d58c43be7d63a9c
ghaeberle <g.haeberle@fu-berlin.de>
2022-06-16 14:55:56 +0200

    added switch for linux mac

--- scripts/plotting/combine_decoding_acc_all_subjects.m
+++ scripts/plotting/combine_decoding_acc_all_subjects.m
@@ -1,8 +1,18 @@
-function [outputArg1,outputArg2] = combine_decoding_acc_all_subjects(inputArg1,inputArg2)
+function [outputArg1,outputArg2] = combine_decoding_acc_all_subjects()
 %% category decoding 
 
-addpath('/Users/ghaeberle/Documents/PhD/project/FixEyeEEG/scripts/EEG/stdshade/');
-out_path = '/Users/ghaeberle/scratch/data/FixEyeEEG/main/results/category_decoding/';
+if ismac
+    addpath('/Users/ghaeberle/Documents/PhD/project/FixEyeEEG/scripts/stats');
+    BASE = '/Users/ghaeberle/scratch/';
+elseif isunix
+    addpath('/home/haebeg19/FixEyeEEG/scripts/stats');
+    BASE = '/scratch/haebeg19/';
+end
+
+out_path = sprintf('%sdata/FixEyeEEG/main/results/category_decoding/',BASE);
+n_subs = 30;
+subs = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32];
+n_timepoints = 240;
 
 if ~isfolder(out_path)
     mkdir(out_path);
@@ -10,18 +20,15 @@ end
 
 methods_flag = ["eeg" "eyetracking"];
 for idx = 1:2
-category_decodingAcc_bulls_all = NaN(30,240);
-category_decodingAcc_standard_all = NaN(30,240);
+category_decodingAcc_bulls_all = NaN(n_subs,n_timepoints);
+category_decodingAcc_standard_all = NaN(n_subs,n_timepoints);
 
-    for subj = 1:30
+    for subj = 1:n_subs
 
-        %if subj == 2 || subj == 4 || subj == 12 
-           % cnt = cnt+1
-           % continue; 
-       % end
-    results_dir = sprintf('/Users/ghaeberle/scratch/data/FixEyeEEG/main/%s/category_decoding/%s', methods_flag(idx), num2str(subj));
-    filename = 'animate_inanimate_category';
-    fileToRead1 = fullfile(results_dir,sprintf('%s_decodingAccuracy_standard.mat',filename));
+        results_dir = sprintf('%sdata/FixEyeEEG/main/%s/category_decoding/%s', BASE, methods_flag(idx), num2str(subs(subj)));
+        filename = 'animate_inanimate_category';
+        fileToRead1 = fullfile(results_dir,sprintf('%s_decodingAccuracy_standard.mat',filename));
+
     
     if exist(fileToRead1, 'file') == 0
       % File does not exist
@@ -43,12 +50,13 @@ category_decodingAcc_standard_all = NaN(30,240);
 end
  %% object decoding
  
-out_path = '/Users/ghaeberle/scratch/data/FixEyeEEG/main/results/object_decoding/';
+out_path = sprintf('%sdata/FixEyeEEG/main/results/object_decoding_all_same_trials/',BASE);
 
 if ~isfolder(out_path)
     mkdir(out_path);
 end
 n_subs = 30;
+subs = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32];
 n_objects = 40;
 n_timepoints = 240;
 methods_flag = ["eeg" "eyetracking"];
@@ -59,7 +67,7 @@ object_decodingAcc_standard_all = NaN(n_subs,n_objects,n_objects,n_timepoints);
 
     for subj = 1:n_subs
         
-    results_dir = sprintf('/Users/ghaeberle/scratch/data/FixEyeEEG/main/%s/object_decoding/%s/', methods_flag(idx), num2str(subj));
+    results_dir = sprintf('%sdata/FixEyeEEG/main/%s/object_decoding_all_same_trials/%s/', BASE, methods_flag(idx), num2str(subs(subj)));
    % filename = 'animate_inanimate_category';
     fileToRead1 = sprintf("%s/objects_bulls_decodingAccuracy.mat", results_dir);
     
