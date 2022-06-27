6b390ffe75511a6a3a8f862021d98a7f281d636c
ghaeberle <g.haeberle@fu-berlin.de>
2022-06-27 15:31:47 +0200

    parallised job

--- scripts/eyetracking/run_R_eyetracking_preprocessing.sh
+++ scripts/eyetracking/run_R_eyetracking_preprocessing.sh
@@ -4,12 +4,35 @@
 #SBATCH --job-name=R_script_eyetracking
 #SBATCH --mail-type=end
 #SBATCH --mem=3000
-#SBATCH --time=11:00:00
+#SBATCH --time=00:40:00
 #SBATCH --qos=standard
+
+declare -a permutations
+index=0
+for sub in 2 3 4 5 6 7 8 9 10 11 12 13 14 15 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31 32 
+do
+    permutations[$index]="$sub" 
+    index=$((index + 1))
+done
+
+#Extract parameters
+params=(${permutations[${SLURM_ARRAY_TASK_ID}]})
+sub=${params[0]}
+
+echo sub $sub
+
 
 module add R/4.0.3-foss-2020b 
 
-Rscript eyetracking_preprocessing_correct_trialinfo_curta.R
+
+sleep 50
+
+cd /home/haebeg19/FixEyeEEG/scripts/eyetracking/
+
+### Start job
+
+Rscript eyetracking_preprocessing_correct_trialinfo_curta.R $sub
+echo set to run
 
 
 
