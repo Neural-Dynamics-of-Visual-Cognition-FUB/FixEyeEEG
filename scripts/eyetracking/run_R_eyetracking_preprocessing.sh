7b598d191f57a306b385752859745e1930f7f28e
Häberle <haebeg19@login.curta.zedat.fu-berlin.de>
2022-06-27 15:29:56 +0200

    script for running r script

--- scripts/eyetracking/run_R_eyetracking_preprocessing.sh
+++ scripts/eyetracking/run_R_eyetracking_preprocessing.sh
@@ -4,10 +4,12 @@
 #SBATCH --job-name=R_script_eyetracking
 #SBATCH --mail-type=end
 #SBATCH --mem=3000
-#SBATCH --time=10:00:00
+#SBATCH --time=11:00:00
 #SBATCH --qos=standard
 
-Rscript eyetracking_preprocessing_correct_trialinfo.R
+module add R/4.0.3-foss-2020b 
+
+Rscript eyetracking_preprocessing_correct_trialinfo_curta.R
 
 
 
