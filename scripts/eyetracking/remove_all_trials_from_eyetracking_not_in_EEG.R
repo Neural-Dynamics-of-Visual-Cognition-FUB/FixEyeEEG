ce31c39d3318223b31fe46dd2dc526331c982325
ghaeberle <g.haeberle@fu-berlin.de>
2022-06-30 14:06:41 +0200

    make sure all trials are the same

---
+++ scripts/eyetracking/remove_all_trials_from_eyetracking_not_in_EEG.R
@@ -0,0 +1,24 @@
+## reject additonal trials from EEG analysis 
+library(dplyr)
+library(data.table)
+library("R.matlab")
+
+subs = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32)
+
+
+# calculate accuracy for all subjects 
+for (sub in subs){
+  raw_trials = paste("/scratch/haebeg19/data/FixEyeEEG/main/eyetracking/preprocessed/raw_correct_trials/raw_sub00",sub,".csv",sep="")
+  raw = read.csv(raw_trials)
+  file_trials_to_keep <- paste('/scratch/haebeg19/data/FixEyeEEG/main/eyetracking/',sub,'/trials_to_keep.mat',sep="")
+  trials = readMat(file_trials_to_keep)
+  trials_to_keep = trials$trials.to.keep
+  df_eyetracking_cleaned_witout_trials_with_missing_timepoints = raw[raw$block %in%  trials_to_keep,]
+  
+  ## remove trials that still contained Nans in baseline and therefore also cannot be used in the eyetracking analysis 
+  
+  
+  
+  write.csv(df_eyetracking_cleaned_witout_trials_with_missing_timepoints, 
+            file=paste("/scratch/haebeg19/data/FixEyeEEG/main/eyetracking/preprocessed/cleaned/eyetracking_cleaned_same_trials_eeg_eyetracking_sub00",sub,".csv",sep=""))
+}
