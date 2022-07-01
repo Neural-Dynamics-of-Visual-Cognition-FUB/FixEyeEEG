33c879d57b193c9b66466576a07e9f0c8cc16d6f
ghaeberle <g.haeberle@fu-berlin.de>
2022-07-01 14:04:17 +0200

    fixed trials

--- scripts/eyetracking/remove_all_trials_from_eyetracking_not_in_EEG.R
+++ scripts/eyetracking/remove_all_trials_from_eyetracking_not_in_EEG.R
@@ -8,7 +8,7 @@ subs = c(2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,2
 
 # calculate accuracy for all subjects 
 for (sub in subs){
-  raw_trials = paste("/scratch/haebeg19/data/FixEyeEEG/main/eyetracking/preprocessed/eyetracking_cleaned_wo_artifacts_and_removed_eeg_trials_and_visual_degree_sub00",sub,".csv",sep="")
+  raw_trials = paste("/scratch/haebeg19/data/FixEyeEEG/main/eyetracking/preprocessed/cleaned/eyetracking_cleaned_wo_artifacts_and_removed_eeg_trials_and_visual_degree_sub00",sub,".csv",sep="")
   raw = read.csv(raw_trials)
   file_trials_to_keep <- paste('/scratch/haebeg19/data/FixEyeEEG/main/eyetracking/preprocessed/',sub,'/timelocked/trials_to_keep.mat',sep="")
   trials = readMat(file_trials_to_keep)
