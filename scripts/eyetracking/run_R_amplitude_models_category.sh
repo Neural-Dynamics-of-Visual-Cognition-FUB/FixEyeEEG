29c8a1b80057e2eaa1ae3b54704e98906bf556ee
ghaeberle <g.haeberle@fu-berlin.de>
2022-07-01 15:16:41 +0200

    scripts to run models on curta

---
+++ scripts/eyetracking/run_R_amplitude_models_category.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#SBATCH --mail-user=haebeg19@zedat.fu-berlin.de
+#SBATCH --job-name=R_script_eyetracking
+#SBATCH --mail-type=end
+#SBATCH --mem=3000
+#SBATCH --time=05::00:00
+#SBATCH --qos=standard
+
+
+module add R/4.0.3-foss-2020b 
+Rscript amplitude_models_category.R
+
+
+
