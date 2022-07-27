5b35bc5434f0d5cae803d223a414b1674e4aef2a
ghaeberle <g.haeberle@fu-berlin.de>
2022-07-27 11:23:34 +0200

    scripts to extract behavioural info

---
+++ scripts/behav/get_behav_info.m
@@ -0,0 +1,39 @@
+function [outputArg1,outputArg2] = get_behav_info()
+if ismac
+    addpath('/Users/ghaeberle/Documents/PhD/project/FixEyeEEG/scripts/decoding');
+    addpath('/Users/ghaeberle/Documents/PhD/project/FixEyeEEG/scripts/decoding/MEG_SVM_decoding_MVNN/');
+    addpath('/Users/ghaeberle/Documents/MATLAB/libsvm/matlab');
+    addpath('/Users/ghaeberle/Documents/MATLAB/fieldtrip-20210928/')
+    ft_defaults
+    BASE = '/Users/ghaeberle/scratch/';
+elseif isunix
+    addpath('/home/haebeg19/FixEyeEEG/scripts/decoding');
+    addpath('/home/haebeg19/FixEyeEEG/scripts/decoding/MEG_SVM_decoding_MVNN/');
+    addpath('/home/haebeg19/toolbox/libsvm/matlab');
+    addpath('/home/haebeg19/toolbox/fieldtrip/')
+    BASE = '/scratch/haebeg19/';
+    ft_defaults
+end
+
+out_path = sprintf('%sdata/FixEyeEEG/main/results/category_train_test/',BASE);
+n_subs = 30;
+subs = [2,3,4,5,6,7,8,9,10,11,12,13,14,15,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32];
+
+if ~isfolder(out_path)
+    mkdir(out_path);
+end
+
+
+    for subj = 1:n_subs
+        load(sprintf('%sdata/FixEyeEEG/main/behav_data/FixCrossExp_s%scfgdata',BASE,num2str(subs(subj))));
+        age(subj) = data.age;
+        gender(subj) = data.gender;
+    end
+    mean(age)
+    std(age)
+    count(gender, 'f')
+    count(gender, 'm')
+    count(gender, 'd')
+    save()
+end
+
